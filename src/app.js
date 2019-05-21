ca949ed223b29f097d69357ae28b556806ddd760
nataliesun <natsun121@gmail.com>
2019-05-21 19:55:18 -0400

    added notes and folders service objects and routers

--- src/app.js
+++ src/app.js
@@ -5,15 +5,21 @@ const cors = require('cors');
 const helmet = require('helmet');
 const { NODE_ENV } = require('./config')
 
+const notesRouter = require('./notes/notes-router')
+const foldersRouter = require('./folders/folders-router')
+
 const app = express();
 
-const morganOption = (NODE_ENV === 'production') 
-    ? 'tiny' 
+const morganOption = (NODE_ENV === 'production')
+    ? 'tiny'
     : 'common';
 
 app.use(morgan(morganOption));
 app.use(cors());
 app.use(helmet());
+
+app.use('/api/notes', notesRouter)
+app.use('/api/folders', foldersRouter)
 
 app.get('/', (req, res) => {
     res.send('Hello, world!')
@@ -22,7 +28,7 @@ app.get('/', (req, res) => {
 app.use(function errorHandler(error, req, res, next) {
     let response
     if (NODE_ENV === 'production') {
-        response = { error: {message: 'server error'} }
+        response = { error: { message: 'server error' } }
     } else {
         console.error(error)
         response = { message: error.message, error }
